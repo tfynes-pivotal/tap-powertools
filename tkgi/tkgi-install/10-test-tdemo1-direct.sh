a84b4e1fd3d24e505430ac16d490374a185dcf43
Mark Fynes <tfynes@vmware.com>
2022-05-18 17:26:01 -0400

    ...

---
+++ tkgi/tkgi-install/10-test-tdemo1-direct.sh
@@ -0,0 +1,1 @@
+while true; do sleep 2 && curl tdemo1-default.homelab.tap.fynesy.com && sleep 2 && clear; done
---
+++ tkgi/tkgi-install/11-test-tdemo1-via-gateway.sh
@@ -0,0 +1,1 @@
+while true; do sleep 2 && curl tdemo1.homelab.tap.fynesy.com && sleep 2 && clear; done
