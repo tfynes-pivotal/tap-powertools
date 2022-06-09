faa3e1d37f13d6e1442912b04f5c434eea1feda4
Mark Fynes <tfynes@vmware.com>
2022-06-09 09:49:19 -0400

    scan test supply chain

--- utils/multi-supplychain/multi-supplychain-installer.sh
+++ utils/multi-supplychain/multi-supplychain-installer.sh
@@ -5,9 +5,18 @@
 #
 # 
 
+
+# Install and configure "Testing" supply chain
 kubectl -n tap-install get secret ootb-supply-chain-basic-values -o jsonpath="{.data.values\.yml}" | base64 -d - > /tmp/values.yaml
 tanzu -n tap-install package install ootb-supply-chain-testing  -p ootb-supply-chain-testing.tanzu.vmware.com  -v 0.7.0 --values-file /tmp/values.yaml
 rm /tmp/values.yaml
 
 
-#kubectl patch clustersupplychains.carto.run source-test-to-url -p '{"spec":{"selector":{"apps.tanzu.vmware.com/workload-type":"webwithtests"}}}' --type 'merge'
+# Install and configure "Testing and Scanning" supply chain - adds 3rd required selector "apps.tanzu.vmware.com/scan-workload":"true"
+kubectl -n tap-install get secret ootb-supply-chain-basic-values -o jsonpath="{.data.values\.yml}" | base64 -d - > /tmp/values.yaml
+tanzu -n tap-install package install ootb-supply-chain-testing-scanning -p ootb-supply-chain-testing-scanning.tanzu.vmware.com -v 0.7.0 --values-file /tmp/values.yaml
+rm /tmp/values.yaml
+
+
+kubectl patch clustersupplychains.carto.run source-test-scan-to-url -p '{"spec":{"selector":{"apps.tanzu.vmware.com/scan-workload":"true"}}}' --type 'merge'
+kubectl patch clustersupplychains.carto.run scanning-image-scan-to-url -p '{"spec":{"selector":{"apps.tanzu.vmware.com/scan-workload":"true"}}}' --type 'merge'
